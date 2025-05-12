af59d72a36bf225da2a27fa4c7b5d1d68c3346ee
LaPockett <dianacarbajal2003@hotmail.com>
2025-05-12 17:58:00 +0200

    feat(update): use fake versions and show alert dialog if user haven't updated

--- composeApp/src/commonMain/kotlin/com/dian/prueba/utilities/UpdateStorage.kt
+++ composeApp/src/commonMain/kotlin/com/dian/prueba/utilities/UpdateStorage.kt
@@ -1,19 +1,53 @@
 package com.dian.prueba.utilities
 
-import com.dian.prueba.model.UpdateInfoUser
+import com.dian.prueba.model.UpdateInfo
 import com.russhwolf.settings.Settings
 import com.russhwolf.settings.set
 
 object UpdateStorage {
-    private const val UPDATE_DONE_KEY = "update_done"
-    private val settings = Settings()
+    private const val UPDATE_AVAILABLE_KEY = "update_available"
+    private const val MUST_UPDATE_KEY = "must_update"
+    private const val NEEDS_UPDATE_KEY = "needs_update"
+    private const val NEW_VERSION_KEY = "new_version"
+    private const val CURRENT_VERSION_KEY = "current_version"
+    val settings = Settings()
 
-    fun setUpdateDone(updateDone: UpdateInfoUser) {
-        settings[UPDATE_DONE_KEY] = updateDone.updateDone
+    fun updateToNewVersion(newVersion: String): UpdateInfo {
+        val updateInfo = UpdateInfo(
+            updateAvailable = false,
+            mustUpdate = false,
+            needsUpdate = false,
+            currentVersion = newVersion,
+            newVersion = newVersion,
+        )
+        saveUpdateAvailable(updateInfo)
+        return updateInfo
+    }
+    fun saveUpdateAvailable(updateInfo: UpdateInfo) {
+        settings[UPDATE_AVAILABLE_KEY] = updateInfo.updateAvailable
+        settings[MUST_UPDATE_KEY] = updateInfo.mustUpdate
+        settings[NEEDS_UPDATE_KEY] = updateInfo.needsUpdate
+        settings[NEW_VERSION_KEY] = updateInfo.newVersion
+        settings[CURRENT_VERSION_KEY] = updateInfo.currentVersion
     }
 
-    fun getUpdateDone(): UpdateInfoUser? {
-        val updateDone = settings.getBooleanOrNull(UPDATE_DONE_KEY) ?: return null
-        return UpdateInfoUser(updateDone)
+
+    fun loadUpdateInfo(): UpdateInfo? {
+        val updateAvailable = settings.getBooleanOrNull(UPDATE_AVAILABLE_KEY)
+        val mustUpdate = settings.getBooleanOrNull(MUST_UPDATE_KEY)
+        val needsUpdate = settings.getBooleanOrNull(NEEDS_UPDATE_KEY)
+        val newVersion = settings.getStringOrNull(NEW_VERSION_KEY)
+        val currentVersion = settings.getStringOrNull(CURRENT_VERSION_KEY)
+        if (updateAvailable != null) {
+            return UpdateInfo(
+                updateAvailable = updateAvailable,
+                mustUpdate = mustUpdate ?: false,
+                needsUpdate = needsUpdate ?: false,
+                newVersion = newVersion ?: "1.3",
+                currentVersion = currentVersion ?: "1.2"
+            )
+        }
+        return null
     }
+
 }
