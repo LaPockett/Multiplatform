6cf7442e9d43a0ae51f4ec61b68421a0cbe1446b
LaPockett <dianacarbajal2003@hotmail.com>
2025-05-12 17:58:00 +0200

    feat(update): use fake versions and show alert dialog if user haven't updated

--- composeApp/src/commonMain/kotlin/com/dian/prueba/network/APIClient.kt
+++ composeApp/src/commonMain/kotlin/com/dian/prueba/network/APIClient.kt
@@ -3,6 +3,7 @@ package com.dian.prueba.network
 import com.dian.prueba.model.Login
 import com.dian.prueba.model.UpdateInfo
 import com.dian.prueba.utilities.Logger
+import com.dian.prueba.utilities.UpdateStorage
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.plugins.contentnegotiation.*
@@ -41,12 +42,20 @@ class APIClient {
             null
         }
     }
+
     fun checkUpdateAvailable(): UpdateInfo {
         logger.warn("Checking for updates...", "checkUpdateAvailable")
         return UpdateInfo(
             updateAvailable = true,
-            mustUpdate = false
+            mustUpdate = true,
+            needsUpdate = false,
+            currentVersion = "1.2",
+            newVersion = "1.3",
         )
+    }
+    fun updateApp() : UpdateInfo {
+        logger.warn("Updating app...", "updateApp")
+        return UpdateStorage.updateToNewVersion("1.3")
     }
 
 }
