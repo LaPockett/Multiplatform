a336629c8d82e6b4c7ded05b4a994872179a4eb7
LaPockett <dianacarbajal2003@hotmail.com>
2025-05-14 10:18:25 +0200

    Revert "refactor(headerManager): Move webViewHeaderManager to repository directory"
    This reverts commit 85636dbfffd14f9d226be11573f33e9d7b3294a5.

---
+++ composeApp/src/commonMain/kotlin/com/dian/prueba/HeaderManager/WebViewHeaderManager.kt
@@ -0,0 +1,27 @@
+package com.dian.prueba.HeaderManager
+
+import com.dian.prueba.utilities.Logger
+
+object WebViewHeaderManager {
+
+    private val headers: MutableMap<String, String> = mutableMapOf()
+    private val logger = Logger("WebViewHeaderManager")
+
+    fun updateLoginCookie(cookie: String) {
+        headers["Cookie"] = "login_cookie=$cookie"
+        logger.debug(headers.toString())
+    }
+    fun getHeaders(): Map<String, String> = headers
+
+    fun updateAccessToken(it: String) {
+        headers["Authorization"] = "BearerAccessToken $it"
+        logger.debug(headers.toString())
+    }
+
+    fun updateRefreshToken(it : String) {
+        headers["Refresh"] = "Bearer $it"
+        logger.debug(headers.toString())
+    }
+
+
+}
