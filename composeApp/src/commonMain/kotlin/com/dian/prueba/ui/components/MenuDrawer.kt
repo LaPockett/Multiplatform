a08cf02c982c39a2a20177887fe4b082696cb6b3
LaPockett <dianacarbajal2003@hotmail.com>
2025-05-14 18:59:56 +0200

    feat(menuDrawer):Implement a MenuDrawer that contains a WebView

--- composeApp/src/commonMain/kotlin/com/dian/prueba/ui/components/MenuDrawer.kt
+++ composeApp/src/commonMain/kotlin/com/dian/prueba/ui/components/MenuDrawer.kt
@@ -1,72 +1,61 @@
 package com.dian.prueba.ui.components
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.*
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material.DrawerState
 import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FabPosition
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import com.dian.prueba.AppNavigation
 import com.dian.prueba.ui.screens.CartScreen
-import com.dian.prueba.ui.screens.SettingsScreen
-import kotlinx.coroutines.launch
 
-/**
- * EN USO
- */
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MenuDrawer(onLogout: () -> Unit) {
-    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-    val scope = rememberCoroutineScope()
-
-    ModalNavigationDrawer(
-        drawerState = drawerState,
-        drawerContent = {
-            ModalDrawerSheet {
-                SettingsScreen()
-            }
-        },
-        gesturesEnabled = drawerState.isOpen,
-        scrimColor = Color.Blue
-    ) {
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = { Text("Menu Drawer") },
-                    navigationIcon = {
-                        IconButton(onClick = {
-                            scope.launch {
-                                drawerState.apply {
-                                    if (isClosed) open() else close()
-                                }
-                            }
-                        }) {
-                            Icon(Icons.Default.Menu, contentDescription = "Menú")
-                        }
-                    }
-                )
-            },
-        ) {
-            Box(modifier = Modifier.fillMaxSize()) {
-                AppNavigation(onLogout = onLogout)
-            }
-        }
+fun MenuDrawer() {
+    val drawerState = rememberDrawerState(DrawerValue.Open)
+    var gesturesEnabled by remember { mutableStateOf(false) }
+    if (drawerState.isOpen){
+        gesturesEnabled = true
+    } else {
+        gesturesEnabled = false
     }
+    ModalNavigationDrawer(
+        drawerContent = {
+            TopAppBarScreen4()
+            //CartScreen()
+        },
+        drawerState = drawerState,
+        scrimColor = Color(0xFF080e45),
+        gesturesEnabled = gesturesEnabled,
+        //modifier = Modifier. ,
+        content = {
+            AppNavigation()
+        }
+    )
 }
 
+@Composable
+fun MenuDrawer2() {
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    ModalNavigationDrawer(
+        drawerContent = {
+            ModalDrawerSheet(
+                drawerContainerColor = Color(0xFF080e45),
+                drawerShape = androidx.compose.material.MaterialTheme.shapes.small
+            ) {
+                CartScreen()
+            }
+        },
+        drawerState = drawerState,
+        scrimColor = Color(0xFF080e45),
+        gesturesEnabled = true,
+        //modifier = Modifier. ,
+        content = {
+            AppNavigation()
+        }
+    )
+}
--- composeApp/src/commonMain/kotlin/com/dian/prueba/ui/components/TopAppBarScreen.kt
+++ composeApp/src/commonMain/kotlin/com/dian/prueba/ui/components/TopAppBarScreen.kt
@@ -3,10 +3,10 @@ package com.dian.prueba.ui.components
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,23 +14,121 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import com.dian.prueba.ui.screens.CartScreen
 import com.dian.prueba.utilities.Logger
 
 // Comando para formatear el código en Android Studio (Linux): Ctrl + Shift + Alt + L
-/**
- * NINGUNO DE ESTOS COMPOSABLES SE USA EN LA APP, pero los dejo aquí por si los llego
- * a necesitar en el futuro
- */
+@Composable
+fun TopAppBarScreen1(
+    onClickDrawer:() -> Unit
+) {
+    var showMenuDrawer by remember { mutableStateOf(false) }
+    val logger = Logger()
 
-/**
- * EN USO
- */
+    TopAppBar(
+        title = {
+            Text(text = "Top App Bar 1 - Amazon")
+        },
+        navigationIcon = {
+            IconButton(onClick = {
+                onClickDrawer()
+            }) {
+                Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+            }
+        }
+    )
+
+}
+@Composable
+fun TopAppBarScreen() {
+    var showMenuDrawer by remember { mutableStateOf(false) }
+    val logger = Logger()
+
+    TopAppBar(
+        title = {
+            Text(text = "Top App Bar 1 - Amazon")
+        },
+        navigationIcon = {
+            IconButton(onClick = {
+                logger.debug("Enseñando el Drawer...", "TopAppBarScreen")
+                showMenuDrawer = true
+            }) {
+                Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+            }
+        }
+    )
+
+    /*Box(Modifier.fillMaxSize()){
+        CartScreen()
+    }*/
+    if (showMenuDrawer) {
+        MenuDrawer2()
+    }
+}
+
+@Composable
+fun TopAppBarScreen2() {
+    val scaffoldState = rememberScaffoldState()
+    var showMenuDrawer = scaffoldState.drawerState.isClosed
+    val logger = Logger()
+    logger.debug("Enseñando el TopAppBar...", "TopAppBarScreen2")
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Top App Bar 2 - Amazon")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        showMenuDrawer = scaffoldState.drawerState.isOpen
+                    }) {
+                        Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+                    }
+                }
+            )
+        }
+
+    ) {
+        Box(Modifier.fillMaxSize()) {
+            CartScreen()
+        }
+    }
+    if (showMenuDrawer) {
+        MenuDrawer()
+    }
+}
+
+@Composable
+fun TopAppBarScreen3() {
+    // scaffoldState = rememberScaffoldState()
+    var showMenuDrawer by remember { mutableStateOf(false) }
+    val logger = Logger()
+    logger.debug("Enseñando el TopAppBar...", "TopAppBarScreen2")
+
+    TopAppBar(
+        title = {
+            Text(text = "Top App Bar 3 - Amazon")
+        },
+        navigationIcon = {
+            IconButton(onClick = {
+                showMenuDrawer = true
+            }) {
+                Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+            }
+        }
+    )
+    if (showMenuDrawer) {
+        MenuDrawer()
+    }
+}
+
 @Composable
 fun TopAppBarScreen4() {
-    val logger = Logger("TopAppBarScreen2")
-    logger.debug("Enseñando el TopAppBar...")
+    // scaffoldState = rememberScaffoldState()
+    var showMenuDrawer by remember { mutableStateOf(false) }
+    val logger = Logger()
+    logger.debug("Enseñando el TopAppBar...", "TopAppBarScreen2")
 
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -41,15 +139,19 @@ fun TopAppBarScreen4() {
         ) {
             TopAppBar(
                 title = {
-                    Text(text = "Shopping Cart- Amazon")
+                    Text(text = "Top App Bar 4 - Amazon")
                 },
                 navigationIcon = {
                     IconButton(onClick = {
+                        showMenuDrawer = true
                     }) {
-                        Icon(imageVector = Icons.Default.ShoppingCart, contentDescription = "Cart")
+                        Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
                     }
                 }
             )
+            if (showMenuDrawer) {
+                MenuDrawer()
+            }
             CartScreen()
         }
 
