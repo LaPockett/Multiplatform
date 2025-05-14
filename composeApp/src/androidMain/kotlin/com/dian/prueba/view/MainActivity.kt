8d81b6f3bfb0db3fd1ac7d1fde1bb25e5bf242b6
LaPockett <dianacarbajal2003@hotmail.com>
2025-05-14 18:59:56 +0200

    feat(menuDrawer):Implement a MenuDrawer that contains a WebView

--- composeApp/src/androidMain/kotlin/com/dian/prueba/view/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/dian/prueba/view/MainActivity.kt
@@ -5,17 +5,36 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import com.dian.prueba.AppLogin
-import com.dian.prueba.ui.components.MenuDrawer
+import com.dian.prueba.HeaderManager.WebViewHeaderManager
+import com.dian.prueba.ui.components.MenuDrawer2
+import com.dian.prueba.viewModel.LoginVM
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    //private val loginViewModel = LoginVM()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //loginViewModel.loadSavedTokens()
+
+        /*lifecycleScope.launch {
+            loginViewModel.tokens.collectLatest { token ->
+                token?.accessToken?.let {
+                    WebViewHeaderManager.updateAccessToken(it)
+                }
+                token?.refreshToken?.let {
+                    WebViewHeaderManager.updateAccessToken(it)
+                    WebViewHeaderManager.updateRefreshToken(it)
+                }
+            }
+        }*/
         setContent {
             //App()
             //AppNavigation()
-            AppLogin()
-            //MenuDrawer()
+            //->AppLogin()
+            MenuDrawer2()
         }
     }
 }
@@ -24,7 +43,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AppAndroidPreview() {
     //AppNavigation()
-    AppLogin()
-    //MenuDrawer()
+    //->AppLogin()
+    MenuDrawer2()
     //App()
 }
--- composeApp/src/commonMain/kotlin/com/dian/prueba/ui/components/TopAppBarScreen.kt
+++ composeApp/src/commonMain/kotlin/com/dian/prueba/ui/components/TopAppBarScreen.kt
@@ -3,10 +3,10 @@ package com.dian.prueba.ui.components
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,10 +18,27 @@ import com.dian.prueba.ui.screens.CartScreen
 import com.dian.prueba.utilities.Logger
 
 // Comando para formatear el código en Android Studio (Linux): Ctrl + Shift + Alt + L
-/**
- * NINGUNO DE ESTOS COMPOSABLES SE USA EN LA APP, pero los dejo aquí por si los llego
- * a necesitar en el futuro
- */
+@Composable
+fun TopAppBarScreen1(
+    onClickDrawer:() -> Unit
+) {
+    var showMenuDrawer by remember { mutableStateOf(false) }
+    val logger = Logger()
+
+    TopAppBar(
+        title = {
+            Text(text = "Top App Bar 1 - Amazon")
+        },
+        navigationIcon = {
+            IconButton(onClick = {
+                onClickDrawer()
+            }) {
+                Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+            }
+        }
+    )
+
+}
 @Composable
 fun TopAppBarScreen() {
     var showMenuDrawer by remember { mutableStateOf(false) }
@@ -40,16 +57,76 @@ fun TopAppBarScreen() {
             }
         }
     )
+
+    /*Box(Modifier.fillMaxSize()){
+        CartScreen()
+    }*/
+    if (showMenuDrawer) {
+        MenuDrawer2()
+    }
+}
+
+@Composable
+fun TopAppBarScreen2() {
+    val scaffoldState = rememberScaffoldState()
+    var showMenuDrawer = scaffoldState.drawerState.isClosed
+    val logger = Logger()
+    logger.debug("Enseñando el TopAppBar...", "TopAppBarScreen2")
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Top App Bar 2 - Amazon")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        showMenuDrawer = scaffoldState.drawerState.isOpen
+                    }) {
+                        Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+                    }
+                }
+            )
+        }
+
+    ) {
+        Box(Modifier.fillMaxSize()) {
+            CartScreen()
+        }
+    }
     if (showMenuDrawer) {
         MenuDrawer()
     }
 }
 
-/**
- * EN USO
- */
+@Composable
+fun TopAppBarScreen3() {
+    // scaffoldState = rememberScaffoldState()
+    var showMenuDrawer by remember { mutableStateOf(false) }
+    val logger = Logger()
+    logger.debug("Enseñando el TopAppBar...", "TopAppBarScreen2")
+
+    TopAppBar(
+        title = {
+            Text(text = "Top App Bar 3 - Amazon")
+        },
+        navigationIcon = {
+            IconButton(onClick = {
+                showMenuDrawer = true
+            }) {
+                Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+            }
+        }
+    )
+    if (showMenuDrawer) {
+        MenuDrawer()
+    }
+}
+
 @Composable
 fun TopAppBarScreen4() {
+    // scaffoldState = rememberScaffoldState()
+    var showMenuDrawer by remember { mutableStateOf(false) }
     val logger = Logger()
     logger.debug("Enseñando el TopAppBar...", "TopAppBarScreen2")
 
@@ -62,15 +139,19 @@ fun TopAppBarScreen4() {
         ) {
             TopAppBar(
                 title = {
-                    Text(text = "Shopping Cart- Amazon")
+                    Text(text = "Top App Bar 4 - Amazon")
                 },
                 navigationIcon = {
                     IconButton(onClick = {
+                        showMenuDrawer = true
                     }) {
-                        Icon(imageVector = Icons.Default.ShoppingCart, contentDescription = "Cart")
+                        Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
                     }
                 }
             )
+            if (showMenuDrawer) {
+                MenuDrawer()
+            }
             CartScreen()
         }
 
