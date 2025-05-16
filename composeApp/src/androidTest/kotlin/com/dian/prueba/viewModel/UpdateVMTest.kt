e58a8b409d422ef26a372f5e6215722480938097
LaPockett <dianacarbajal2003@hotmail.com>
2025-05-16 18:57:40 +0200

    working with Test. In progress...

---
+++ composeApp/src/androidTest/kotlin/com/dian/prueba/viewModel/UpdateVMTest.kt
@@ -0,0 +1,26 @@
+package com.dian.prueba.viewModel
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+ class UpdateVMTest {
+
+@Before
+ fun setUp() {}
+
+@After
+ fun tearDown() {}
+
+@Test
+ fun getUpdateInfo() {}
+
+@Test
+ fun getShowUpdateDialog() {}
+
+@Test
+ fun checkForUpdates() {}
+
+@Test
+ fun doUpdate() {}
+}
