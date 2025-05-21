10f8bef7aa3ef9d215b1ec211f57c3c0ce6a6944
LaPockett <dianacarbajal2003@hotmail.com>
2025-05-21 18:17:55 +0200

    test(loginVM): Test Login in progress and add MapSettings in TokenStorage

--- composeApp/src/commonMain/kotlin/com/dian/prueba/utilities/TokenStorage.kt
+++ composeApp/src/commonMain/kotlin/com/dian/prueba/utilities/TokenStorage.kt
@@ -1,35 +1,33 @@
 package com.dian.prueba.utilities
 
 import com.dian.prueba.model.Tokens
+import com.russhwolf.settings.MapSettings
 import com.russhwolf.settings.set
 import com.russhwolf.settings.Settings
 
-interface TokenStorage {
-    fun saveTokens(tokens: Tokens)
-    fun loadTokens(): Tokens?
-    fun clear()
-}
-class TokenStorageImpl(private val settings: Settings) : TokenStorage {
-    private companion object {
-        const val ACCESS_TOKEN_KEY = "access_token"
-        const val REFRESH_TOKEN_KEY = "refresh_token"
+object TokenStorage {
+    private const val ACCESS_TOKEN_KEY = "access_token"
+    private const val REFRESH_TOKEN_KEY = "refresh_token"
+
+    private lateinit var _settings: Settings
+    val settings: Settings = MapSettings()
+
+    fun init(settings: Settings) {
+        _settings = settings
     }
 
-    override fun saveTokens(tokens: Tokens) {
+    fun saveTokens(tokens: Tokens) {
         settings[ACCESS_TOKEN_KEY] = tokens.accessToken
-        tokens.refreshToken?.let {
+        settings[REFRESH_TOKEN_KEY] = tokens.refreshToken  //
+        /*tokens.refreshToken?.let {
             settings[REFRESH_TOKEN_KEY] = it
-        }
+        }*/
     }
 
-    override fun loadTokens(): Tokens? {
+    fun loadTokens(): Tokens? {
         val access = settings.getStringOrNull(ACCESS_TOKEN_KEY) ?: return null
         val refresh = settings.getStringOrNull(REFRESH_TOKEN_KEY)
         return Tokens(access, refresh)
     }
 
-    override fun clear() {
-        settings.remove(ACCESS_TOKEN_KEY)
-        settings.remove(REFRESH_TOKEN_KEY)
-    }
 }
