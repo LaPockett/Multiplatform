de2c2c47a9e1805bd86cb29eacc211fafa052413
LaPockett <dianacarbajal2003@hotmail.com>
2025-05-21 14:35:18 +0200

    refactor(loginvmtest): delete the .kt

--- composeApp/src/commonTest/kotlin/common/example/search/LoginVMTest.kt
+++
@@ -1,9 +0,0 @@
-package common.example.search
-
-import com.dian.prueba.utilities.Logger
-import com.dian.prueba.viewModel.LoginVM
-
-class LoginVMTest {
-    private lateinit var viewModel: LoginVM
-    private val logger = Logger()
-}
