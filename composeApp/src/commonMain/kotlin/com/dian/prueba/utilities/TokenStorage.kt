d71f806fb94e4150d39b01e5b799a709c3dba361
LaPockett <dianacarbajal2003@hotmail.com>
2025-05-23 18:01:13 +0200

    refactor(viewmodel, storage): introduce interfaces for dependencies

--- composeApp/src/commonMain/kotlin/com/dian/prueba/utilities/TokenStorage.kt
+++ composeApp/src/commonMain/kotlin/com/dian/prueba/utilities/TokenStorage.kt
@@ -1,33 +1,35 @@
 package com.dian.prueba.utilities
 
 import com.dian.prueba.model.Tokens
-import com.russhwolf.settings.MapSettings
 import com.russhwolf.settings.set
 import com.russhwolf.settings.Settings
 
-object TokenStorage {
-    private const val ACCESS_TOKEN_KEY = "access_token"
-    private const val REFRESH_TOKEN_KEY = "refresh_token"
-
-    private lateinit var _settings: Settings
-    val settings: Settings = MapSettings()
-
-    fun init(settings: Settings) {
-        _settings = settings
+interface TokenStorage {
+    fun saveTokens(tokens: Tokens)
+    fun loadTokens(): Tokens?
+    fun clear()
+}
+class TokenStorageImpl(private val settings: Settings) : TokenStorage {
+    private companion object {
+        const val ACCESS_TOKEN_KEY = "access_token"
+        const val REFRESH_TOKEN_KEY = "refresh_token"
     }
 
-    fun saveTokens(tokens: Tokens) {
+    override fun saveTokens(tokens: Tokens) {
         settings[ACCESS_TOKEN_KEY] = tokens.accessToken
-        settings[REFRESH_TOKEN_KEY] = tokens.refreshToken  //
-        /*tokens.refreshToken?.let {
+        tokens.refreshToken?.let {
             settings[REFRESH_TOKEN_KEY] = it
-        }*/
+        }
     }
 
-    fun loadTokens(): Tokens? {
+    override fun loadTokens(): Tokens? {
         val access = settings.getStringOrNull(ACCESS_TOKEN_KEY) ?: return null
         val refresh = settings.getStringOrNull(REFRESH_TOKEN_KEY)
         return Tokens(access, refresh)
     }
 
+    override fun clear() {
+        settings.remove(ACCESS_TOKEN_KEY)
+        settings.remove(REFRESH_TOKEN_KEY)
+    }
 }
