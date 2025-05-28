8332420dc5c039bed5e22a89c644689382d635bb
LaPockett <dianacarbajal2003@hotmail.com>
2025-05-28 13:30:18 +0200

    refactor(tests): the tests moved to the same project package and remove call api in FakeApiService

---
+++ composeApp/src/test/java/com/dian/prueba/viewModel/FakeApiService.kt
@@ -0,0 +1,31 @@
+package com.dian.prueba.viewModel
+
+import com.dian.prueba.model.UpdateInfo
+import com.dian.prueba.network.ApiService
+
+/**
+ * FakeApiService para uso de LoginVMTest y UpdateVMTest
+ * Anotaciones:
+ * - En mocking nunca se llaman a APIs ni recursos externos, lo que se hace
+ *   simular una respuesta falsa
+ */
+class FakeApiService : ApiService {
+
+    // Para UpdateVMTest
+    override fun checkUpdateAvailable(): UpdateInfo {
+        return UpdateInfo(
+            mustUpdate = true,
+            currentVersion = "1.2",
+            newVersion = "1.3"
+        )
+    }
+
+    // Para LoginVMTest
+    override suspend fun requestLogin(id: String): String? {
+        return if (id.toInt() in 1..10) {
+            "Sincere@april.biz"
+        } else {
+            null
+        }
+    }
+}
