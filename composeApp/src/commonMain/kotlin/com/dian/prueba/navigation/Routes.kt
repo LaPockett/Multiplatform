1fb04105ca0a3f6c87cb70bcac75ca83fec5a705
LaPockett <dianacarbajal2003@hotmail.com>
2025-06-02 18:24:22 +0200

    feat(logout): add logout button and clearSavedTokens (fix navigation to LoginScreen)

--- composeApp/src/commonMain/kotlin/com/dian/prueba/navigation/Routes.kt
+++ composeApp/src/commonMain/kotlin/com/dian/prueba/navigation/Routes.kt
@@ -5,5 +5,6 @@ sealed class Screen (val route: String){
     object Cart: Screen("cart")
     object Profile: Screen("profile")
     object Explore: Screen("explore")
+    object Login : Screen("login")
     object AppNavigation : Screen("AppNavigation")
 }
--- composeApp/src/commonMain/kotlin/com/dian/prueba/ui/screens/ProfileScreen.kt
+++ composeApp/src/commonMain/kotlin/com/dian/prueba/ui/screens/ProfileScreen.kt
@@ -1,18 +1,43 @@
 package com.dian.prueba.ui.screens
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.dian.prueba.network.APIClient
+import com.dian.prueba.repository.LoginRepositoryImpl
 import com.dian.prueba.ui.components.WebViewAccount
+import com.dian.prueba.utilities.TokenStorageImpl
+import com.dian.prueba.utilities.UpdateStorageImpl
+import com.dian.prueba.viewModel.LoginVM
+import com.russhwolf.settings.Settings
 
 @Composable
 fun ProfileScreen(navController: NavController){
+    val loginViewModel = remember {
+        LoginVM(
+            loginRepository = LoginRepositoryImpl(
+                apiService = APIClient(
+                    updateStorage = UpdateStorageImpl(
+                        settings = Settings()
+                    )
+                )
+            ),
+            tokenStorage = TokenStorageImpl(
+                settings = Settings()
+            )
+        )
+    }
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
@@ -31,6 +56,23 @@ fun ProfileScreen(navController: NavController){
 
             ) {
             Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back", tint = Color.White)
+        }
+        Button(
+            onClick = {
+                loginViewModel.clearSavedTokens()
+                // Arreglar esto para que el usuario no pueda salir del Login y para que
+                // no salgan los elementos del bottomNavigation
+                navController.navigate(route= "login")
+            },
+            colors = ButtonDefaults.buttonColors(
+                // Color crema #b7af98
+                backgroundColor = Color.Red,
+                contentColor = Color.White
+            ),
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier.align(Alignment.BottomCenter).padding(bottom =5.dp)
+        ) {
+            Text("Cerrar sesión")
         }
     }
 }
