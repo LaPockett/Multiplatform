829bcc74ea214e8b0d19afff493c9662c119acd1
LaPockett <dianacarbajal2003@hotmail.com>
2025-06-03 11:39:09 +0200

    feat(UI): Customize transparent StatusBar with edge-to edge support - Implement transparent status bar for both light/dark themes - Ensure status bar icons adapt to theme (black for light theme) - Migrate BottomNavigationBar to Material3 A

--- composeApp/src/androidMain/kotlin/com/dian/prueba/view/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/dian/prueba/view/MainActivity.kt
@@ -2,16 +2,45 @@ package com.dian.prueba.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.dian.prueba.AppLogin
-import com.dian.prueba.ui.components.MenuDrawer
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
+            //enableEdgeToEdge()
+            val darkColor = Color.Transparent
+            val lightColor = Color.Transparent
+            val isDarkTheme = isSystemInDarkTheme()
+            enableEdgeToEdge(
+                statusBarStyle = if (!isDarkTheme){
+                    SystemBarStyle.dark(darkColor.hashCode())
+                } else SystemBarStyle.light(lightColor.hashCode(), lightColor.hashCode()),
+                navigationBarStyle = if (!isDarkTheme){
+                    SystemBarStyle.dark(darkColor.hashCode())
+                } else SystemBarStyle.light(lightColor.hashCode(), lightColor.hashCode())
+            )
+            /**
+             * Esto es para que los datos del status bar se vean dependiendo del tema
+             */
+            val view= LocalView.current
+            SideEffect {
+                val window = (view.context as ComponentActivity).window
+                window.statusBarColor = Color.Transparent.toArgb()
+                WindowCompat.getInsetsController(window,view).isAppearanceLightStatusBars = !isDarkTheme
+            }
             //App()
             //AppNavigation()
             AppLogin()
