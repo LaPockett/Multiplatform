4edba1680946cf816ec76854abc6f2610c51ecb6
LaPockett <dianacarbajal2003@hotmail.com>
2025-06-05 11:31:53 +0200

    test(appium): testSucessfulLogin completed

--- appium/testSuccessfulLogin.js
+++ appium/testSuccessfulLogin.js
@@ -12,13 +12,18 @@ const capabilities = {
 const wdOpts = {
     hostname: process.env.APPIUM_HOST || 'localhost',
     port: parseInt(process.env.APPIUM_PORT, 10) || 4723,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
     logLevel: 'info',
     capabilities,
 };
 
 async function runTestLogin() {
-    const driver = await remote(wdOpts);
+    const KEYCODE_MULTITAREA = 187;
+    let driver;
+
     try {
+        driver = await remote(wdOpts);
         await driver.pause(1000);
 
         const emailField = await driver.$('//androidx.compose.ui.platform.ComposeView/android.view.View/android.widget.EditText[1]');
@@ -43,30 +48,63 @@ async function runTestLogin() {
             updateButton.click();
             await driver.pause(3000);
 
+            await driver.pressKeyCode(KEYCODE_MULTITAREA);
+            await driver.pause(1000);
+
             const app = await driver.$('//android.widget.FrameLayout[@content-desc="Multiplatform,Desbloqueado"]')
-            const buttonMenu = await driver.$('(//android.view.View[@resource-id="com.miui.home:id/task_view_thumbnail"])[3]')
-            buttonMenu.click();
-            await driver.pause(3000);
+            await driver.pause(1500);
             app.click();
-            await driver.pause(3000);
+            await driver.pause(1500);
             const mainScreen = await driver.$('//androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[1]')
             if (await mainScreen.isDisplayed()) {
                 console.log('Test: Pantalla principal visible OK');
+                const profileScreen = await driver.$('//androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.view.View/android.view.View[2]')
+                const cartScreen = await driver.$('//androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.view.View/android.view.View[3]')
+                const exploreScreen = await driver.$('//androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View[4]')
+                const buttonBack = await driver.$('//androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[1]/android.view.View[3]/android.widget.Button')
+                await profileScreen.waitForDisplayed({timeout: 5000});
+                profileScreen.click();
+                await buttonBack.waitForDisplayed({timeout: 5000});
+                buttonBack.click();
+
+                await cartScreen.waitForDisplayed({timeout: 5000});
+                cartScreen.click();
+                await exploreScreen.waitForDisplayed({timeout: 5000});
+                exploreScreen.click();
+                await driver.pause(2000);
+
+                console.log("Test completado con éxito.");
             } else {
                 throw new Error('La pantalla principal no es visible');
             }
+            testCompleted = true;
         } else {
             throw new Error('La alerta de actualización no es visible');
         }
         
     } catch (error) {
         console.error('Error en el test:', error);
-        const screenshot = await driver.takeScreenshot();
-        require('fs').writeFileSync('screenshot_error.png', screenshot, 'base64');
+        if (driver) {
+            try {
+                const screenshot = await driver.takeScreenshot();
+                require('fs').writeFileSync('screenshot_error.png', screenshot, 'base64');
+                console.log('Captura de pantalla guardada en screenshot_error.png');
+            } catch (screenshotError) {
+                console.error('Error al guardar la captura de pantalla:', screenshotError);
+            }
+        }
+
         throw error;
 
     } finally {
-        await driver.deleteSession();
+        if (driver){
+            try {
+                await driver.deleteSession();
+                console.log('Sesión eliminada con éxito');
+            } catch (error) {
+                console.error('Error al eliminar la sesión:', error);
+            }
+        }
     }
 }
 
