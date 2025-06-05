02bbb5c61915bf8758ddcb72d49196dbc9f9dfd0
LaPockett <dianacarbajal2003@hotmail.com>
2025-06-05 15:05:29 +0200

    test(appium): Centralize all tests in one file and add testMenuDrawer

--- appium/testAll.js
+++ appium/testAll.js
@@ -8,6 +8,7 @@ const capabilities = {
     platformName: 'Android',
     'appium:automationName': 'UiAutomator2',
     'appium:deviceName': 'Android',
+    'appium:udid': '1e27529b',
     'appium:appPackage': 'com.dian.prueba',
     'appium:appActivity': 'com.dian.prueba.view.MainActivity',
     'appium:autoGrantPermissions': true,
@@ -29,25 +30,23 @@ async function runAllTests() {
 
         // 1. Test login vacío
         await emptyLogin.runTestLogin(driver);
-        await driver.pause(1000);
+        await driver.pause(2000);
         await driver.terminateApp('com.dian.prueba');
         await driver.activateApp('com.dian.prueba');
+
         //2. Test login no válido
         await unsuccessfulLogin.runTestLogin(driver);
-        await driver.pause(1000);
+        await driver.pause(2000);
         await driver.terminateApp('com.dian.prueba');
         await driver.activateApp('com.dian.prueba');
-        //Los comento porque si no da error en los actions porque
-        //se usa un emulador y ciertos elementos no son lo mismo que mi
-        // dispositivo real
+
         // 3. Test login válido
-        //await driver.pause(1000);
-        //await successfulLogin.runTestLogin(driver);
+        await driver.pause(2000);
+        await successfulLogin.runTestLogin(driver);
 
         // 4. Test menú drawer (swipe)
-        //await driver.pause(2000);
-        //await menuDrawer.runTestLogin(driver);
-        //await driver.pause(1500);
+        await driver.pause(3000);
+        await menuDrawer.runTestLogin(driver);
 
     } catch (error) {
         console.error('Error en un test :', error);
