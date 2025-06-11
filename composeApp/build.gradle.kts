88475860453f3f461b0339cf267e1db80860a04d
LaPockett <dianacarbajal2003@hotmail.com>
2025-06-11 16:10:55 +0200

    github action: update main

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -128,7 +128,7 @@ android {
     compileSdk = libs.versions.android.compileSdk.get().toInt()
     signingConfigs {
         create("release") {
-            storeFile = file("composeApp/keydemiapp.jks")
+            storeFile = file("keydemiapp.jks")
             storePassword = System.getenv("ORG_GRADLE_PROJECT_storePassword")?: ""
             keyAlias = System.getenv("ORG_GRADLE_PROJECT_keyAlias")?: ""
             keyPassword = System.getenv("ORG_GRADLE_PROJECT_keyPassword")?: ""
