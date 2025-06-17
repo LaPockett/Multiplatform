48ffae23b9909fe1a84355eacee8d16a3de09528
LaPockett <dianacarbajal2003@hotmail.com>
2025-06-17 14:50:42 +0200

    github-action: appium

--- scripts/second_emulator.sh
+++ scripts/second_emulator.sh
@@ -1,4 +1,5 @@
-            emulator -list-avds
-            emulator -avd Pixel_8_Pro
-            adb install /home/usuario/Descargas/upload/composeApp-release.apk
-            npm run test-all
+#! /bin/bash
+emulator -list-avds
+emulator -avd Pixel_8_Pro
+adb install /home/usuario/Descargas/upload/composeApp-release.apk
+npm run test-all
