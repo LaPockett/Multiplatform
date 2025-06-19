798741662d0af65714dcbf1644b15b1b806a5d3f
LaPockett <dianacarbajal2003@hotmail.com>
2025-06-19 14:32:29 +0200

    update: fix scroll row and change scrimColor MenuDrawer

--- composeApp/src/commonMain/kotlin/com/dian/prueba/ui/components/MenuDrawer.kt
+++ composeApp/src/commonMain/kotlin/com/dian/prueba/ui/components/MenuDrawer.kt
@@ -2,12 +2,10 @@ package com.dian.prueba.ui.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.*
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalDrawerSheet
@@ -21,7 +19,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.dian.prueba.AppNavigation
-import com.dian.prueba.ui.screens.CartScreen
 import com.dian.prueba.ui.screens.SettingsScreen
 import kotlinx.coroutines.launch
 
@@ -43,7 +40,7 @@ fun MenuDrawer(onLogout: () -> Unit) {
             }
         },
         gesturesEnabled = drawerState.isOpen,
-        scrimColor = Color.Blue
+        scrimColor = Color(0xff0e1147)
     ) {
         Scaffold(
             topBar = {
