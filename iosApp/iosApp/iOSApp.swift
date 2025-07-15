8f78aaf21fd55e3f733853eb6fa5b8eb7528999d
diandev <dianacarbajal2003@hotmail.com>
2025-07-15 12:19:00 +0200

    feat(fcm): add fcm configurations for test use in iOS
    For test notifications to work, you need to generate an APN's key, which you can obtain through the Apple Developer Portal

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,12 +1,28 @@
 import SwiftUI
 import FirebaseCore
+import FirebaseMessaging
 
-class AppDelegate: NSObject, UIApplicationDelegate {
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     FirebaseApp.configure()
+      UNUserNotificationCenter.current().delegate = self
+      let authOptions : UNAuthorizationOptions = [.alert, .badge, .sound]
+      UNUserNotificationCenter.current().requestAuthorization(options: authOptions){ (_, error) in
+          guard error == nil else {
+              print(error!.localizedDescription)
+              return
+          }
+      }
+      application.registerForRemoteNotifications()
     return true
   }
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String){
+        print("Firebase registration token: $fcmToken")
+        print("\(#function)\nFirebase registration token: \(String(describing: fcmToken))")
+        let dataDict:[String:String] = ["token": fcmToken]
+        NotificationCenter.default.post(name: Notification.Name("FCMToken"), object:nil, userInfo:dataDict)
+    }
 }
 
 @main
