18e34eedb109467938a06ef5963a897e2f978c3e
diandev <dianacarbajal2003@hotmail.com>
2025-07-23 17:43:10 +0200

    refactor(ui): set transparent safe areas in iOS
    change background color on MenuDrawer

--- composeApp/src/commonMain/kotlin/com/dian/prueba/App.kt
+++ composeApp/src/commonMain/kotlin/com/dian/prueba/App.kt
@@ -6,12 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -91,7 +86,7 @@ fun App() {
                         },
                         colors = ButtonDefaults.buttonColors(
                             contentColor = Color(0xffeaf7f8),
-                            backgroundColor = Color(0xff7265da)
+                            containerColor = Color(0xff7265da)
                         )
                     ) {
                         Text("Next activity")
