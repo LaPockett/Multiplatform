68df6154a3bc923381b00b16fc7e92983a3899f2
LaPockett <dianacarbajal2003@hotmail.com>
2026-01-16 10:12:07 +0100

    chore: change main screen of iosMain

--- composeApp/src/iosMain/kotlin/com/dian/prueba/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/dian/prueba/MainViewController.kt
@@ -1,5 +1,6 @@
 package com.dian.prueba
 import androidx.compose.ui.window.ComposeUIViewController
 import com.dian.prueba.ui.screens.RootNavigation
+import com.dian.prueba.ui.splash.CentralSplashScreen
 
-fun MainViewController() = ComposeUIViewController { RootNavigation() }
+fun MainViewController() = ComposeUIViewController { CentralSplashScreen() }
