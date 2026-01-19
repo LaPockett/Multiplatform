eee47e49520904fd4c191b674a07c572f1ed82c0
LaPockett <dianacarbajal2003@hotmail.com>
2026-01-19 14:26:05 +0100

    style: build launch screen like a composable (in progress)

---
+++ composeApp/src/commonMain/kotlin/com/dian/prueba/ui/splash/LaunchScreen.kt
@@ -0,0 +1,61 @@
+package com.dian.prueba.ui.splash
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.dian.prueba.model.LocalColors
+import io.github.alexzhirkevich.compottie.LottieCompositionSpec
+import io.github.alexzhirkevich.compottie.animateLottieCompositionAsState
+import io.github.alexzhirkevich.compottie.rememberLottieComposition
+import io.github.alexzhirkevich.compottie.rememberLottiePainter
+import kotlinx.coroutines.delay
+import multiplatform.composeapp.generated.resources.Res
+
+/**
+ * In progress
+ */
+@Composable
+fun LogoLottie(
+) {
+    val colorModifier = LocalColors.current
+    val composition by rememberLottieComposition {
+        LottieCompositionSpec.JsonString(
+            Res.readBytes("files/logo_launch.json").decodeToString()
+        )
+    }
+    val progress by animateLottieCompositionAsState(composition)
+    var showLottie by remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit) {
+        delay(1000)
+        showLottie = true
+        delay(1200)
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorModifier.backgroundApp),
+        contentAlignment = Alignment.Center
+    ) {
+        SmoothAppear(visible = showLottie) {
+            Image(
+                painter = rememberLottiePainter(
+                    composition = composition,
+                    progress = { progress },
+                ),
+                contentDescription = "Lottie animation"
+            )
+        }
+
+    }
+}
