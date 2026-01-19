f95ea606001f2ce13a4d19e078aa52d31164b9f6
LaPockett <dianacarbajal2003@hotmail.com>
2026-01-19 11:08:47 +0100

    chore: delete invalid import in iosMain

--- composeApp/src/iosMain/kotlin/com/dian/prueba/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/dian/prueba/MainViewController.kt
@@ -1,6 +1,5 @@
 package com.dian.prueba
 import androidx.compose.ui.window.ComposeUIViewController
-import com.dian.prueba.ui.screens.RootNavigation
 import com.dian.prueba.ui.splash.CentralSplashScreen
 
 fun MainViewController() = ComposeUIViewController { CentralSplashScreen() }
