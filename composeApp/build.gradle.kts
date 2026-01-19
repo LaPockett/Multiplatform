eaeeedb09e5c418831912e44f443ffaa1cc779cc
LaPockett <dianacarbajal2003@hotmail.com>
2026-01-19 12:37:11 +0100

    build: comment chaintech media player dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -130,8 +130,8 @@ kotlin {
             implementation("io.lapockett:cmpglass:1.1.0")
             // Ref: https://stackoverflow.com/questions/79340066/backhandler-on-compose-multiplatform-android-and-ios
             implementation("org.jetbrains.compose.ui:ui-backhandler:1.9.2")
-            // CMP - Media Player (probar con móvil físico)
-            implementation("network.chaintech:compose-multiplatform-media-player:1.0.52")
+            // CMP - Media Player
+            //implementation("network.chaintech:compose-multiplatform-media-player:1.0.52")
             // CMP - Media PLayer de kdroidFilter
             implementation("io.github.kdroidfilter:composemediaplayer:0.8.7")
         }
